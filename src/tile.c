4e71b5d5a6e46383e93c3779d574897540c22165
tomspex <tboxtungsten@gmail.com>
2024-05-22 07:14:31 -0400

    major tilemap rendering optimization

--- src/tile.c
+++ src/tile.c
@@ -35,21 +35,24 @@ void TileMapRead(TileMap *tileMap, const char *src) {
 }
 
 void TileMapRender(TileMap *tileMap, Entity *camera, SDL_Renderer *renderer) {
-
+    
     int cameraX = round(camera->collision->x),
-        cameraY = round(camera->collision->y);
+        cameraY = round(camera->collision->y),
+        cameraMapX = cameraX/TILE_WIDTH,
+        cameraMapY = cameraY/TILE_HEIGHT,
+        cameraMapW = camera->collision->w/TILE_WIDTH,
+        cameraMapH = camera->collision->h/TILE_HEIGHT+1;
     SDL_Rect 
         tileSrc = {0, 0, TILE_WIDTH, TILE_HEIGHT},
         tileDest = {0, 0, TILE_WIDTH, TILE_HEIGHT};
-
-    for(int i=0;i<tileMap->size;i++) {
-        if(tileMap->tiles[i].spriteID != 0) {
-            tileDest.x = tileMap->tiles[i].position.x*TILE_WIDTH;
-            tileDest.y = tileMap->tiles[i].position.y*TILE_HEIGHT;
-	    if(SDL_HasIntersection(&tileDest, camera->collision)) {
-                tileDest.x -= cameraX;
-                tileDest.y -= cameraY;
-                tileSrc.x = (tileMap->tiles[i].spriteID-1)*TILE_WIDTH; // subtract one to account for null value
+    
+    for(int i=0;i<=cameraMapH;i++) {
+        for(int j=0;j<=cameraMapW;j++) {
+            int index = cameraMapX+j+(cameraMapY+i)*tileMap->width;
+            if(tileMap->tiles[index].spriteID != 0) {
+                tileDest.x = tileMap->tiles[index].position.x*TILE_WIDTH-cameraX;
+                tileDest.y = tileMap->tiles[index].position.y*TILE_HEIGHT-cameraY;
+                tileSrc.x = (tileMap->tiles[index].spriteID-1)*TILE_WIDTH; // subtract one to account for null value
                 SDL_RenderCopy(
                     renderer,
                     tileMap->spritesheet,
